ef6056eaa4272689a5c329246fd04b8a083cffa2
proqen <45311452+proqen@users.noreply.github.com>
2021-01-27 18:05:00 +0600

    create migration

--- main/views.py
+++ main/views.py
@@ -5,3 +5,12 @@ def homepage(request):
     
 def test(request):
     return render(request, "test.html")
+
+def addToDo(request):
+    return render(request, "addToDo.html")
+
+def removeToDo(request):
+    return render(request, "removeToDo.html")
+
+def updateToDo(request):
+    return render(request, "updateToDo.html")
--- todo/urls.py
+++ todo/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test
-from product.views import go, third
+from main.views import *
+from product.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -25,6 +25,9 @@ urlpatterns = [
     path("", homepage, name="home"),
     path("test/", test, name = "test"),
     path("go/", go, name = "go"),
-    path("test3/", third, name = "test3")
+    path("test3/", third, name = "test3"),
+    path("add/", addToDo),
+    path("remove/", removeToDo),
+    path("update/", updateToDo)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
